ecc4fe1f1f5a6fafe40202858473b4302c9e7140
Anitha Rayala <rayalaanitha2@gmail.com>
2026-07-07 12:22:08 +0530

    Updated settings for Render deployment

--- movie_booking/settings.py
+++ movie_booking/settings.py
@@ -9,6 +9,7 @@ https://docs.djangoproject.com/en/6.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/6.0/ref/settings/
 """
+import os
 
 from pathlib import Path
 
@@ -20,12 +21,19 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-2*w4998$%@2t2+tsiz3)ij=x&mkif$a-rjau#onmnkzioy#cch'
+SECRET_KEY = os.environ.get(
+    "SECRET_KEY",
+    "django-insecure-2*w4998$%@2t2+tsiz3)ij=x&mkif$a-rjau#onmnkzioy#cch"
+)
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    ".onrender.com",
+    "127.0.0.1",
+    "localhost",
+]
 
 
 # Application definition
@@ -120,3 +128,5 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
+
+STATIC_ROOT = BASE_DIR / "staticfiles"
